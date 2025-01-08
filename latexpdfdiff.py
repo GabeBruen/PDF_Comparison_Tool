c1b4c787e67d88a2c44878af8f61a0c0ef09dc92
GabeBruen <158603574+GabeBruen@users.noreply.github.com>
2025-01-08 00:54:36 -0600

    Update latexpdfdiff.py

--- latexpdfdiff.py
+++ latexpdfdiff.py
@@ -3,7 +3,7 @@
 """
 AI code was used as a base to help me get started, but
 I added on to the code to make it more user friendly, both for
-the user and anyone looking at this code.
+the user and anyone looking at this code. -GVB
 """
 
 import os
